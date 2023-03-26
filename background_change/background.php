697a3a0d228f9c0b4a485a3068e1de5e3ac75ea4
judithkurian-12 <69835510+judithkurian-12@users.noreply.github.com>
2023-03-26 23:48:27 +0530

    minor changes

--- background_change/background.php
+++ background_change/background.php
@@ -1,7 +1,5 @@
 <?php
 $currentTime = date('H:i:s');
-// echo $currentTime;
-// exit();
 $morningStart = date('H:i:s', strtotime('03:00:00'));
 $morningEnd = date('H:i:s', strtotime('11:59:59'));
 $afternoonStart = date('H:i:s', strtotime('12:00:00'));
@@ -11,7 +9,6 @@ $eveningEnd = date('H:i:s', strtotime('19:59:59'));
 $nightStart = date('H:i:s', strtotime('20:00:00'));
 $nightEnd = date('H:i:s', strtotime('02:59:59'));
 
-$image;
 $fontColor= 'black';
 if($currentTime > $morningStart && $currentTime < $morningEnd)
 {
@@ -38,15 +35,6 @@ else
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- Bootstrap CSS -->
-
-        <!-- <link rel="stylesheet" href="/manage/features/eod/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/manage/bootstrap_tablesorter/css/theme.bootstrap_3.css" />
-
-        <script src="/manage/features/eod/js/jquery.min.js"></script>
-        <script src="/manage/features/eod/js/jquery-3.1.1.min.js" ></script>
-        <script type="text/javascript" src="/manage/javascript/jquery-ui.js"></script>
-        <script src="/manage/features/eod/js/bootstrap.min.js"></script> -->
 
         <title>Background Change</title>
 
